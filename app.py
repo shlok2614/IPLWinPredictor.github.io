572eb6fd10797e743a131423579f2e4f71a69b06
shlok2614 <shloksg2614@gmail.com>
2026-03-16 21:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,12 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
 
+if os.path.exists("pipe.pkl"):
+    pipe = pickle.load(open("pipe.pkl","rb"))
+else:
+    st.error("Model file pipe.pkl not found")
+ 
 
-
-pipe = pickle.load(open('pipe.pkl','wb'))
 st.title('IPL Win Predictor')
 
 col1, col2 = st.columns(2)
